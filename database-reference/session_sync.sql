f2bf28d09bebf426bb3ad1a290c44c65f3f52d52
emarmolnet-glitch <emarmolnet@gmail.com>
2026-07-11 08:16:30 +0200

    Cleanup: Remove unused netlify database directory

--- database-reference/session_sync.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS session_sync (
-  user_id TEXT PRIMARY KEY,
-  last_sync_data JSONB NOT NULL CHECK (jsonb_typeof(last_sync_data) = 'object'),
-  last_action_module TEXT NOT NULL,
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
