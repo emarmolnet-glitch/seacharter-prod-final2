83d34200f21fbec9450aaf3357b7f0186a86e0d9
emarmolnet-glitch <emarmolnet@gmail.com>
2026-09-08 18:28:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ const legacyAssets = [
   "_headers",
   "_redirects",
   "contract-reference.js",
-  "src="./network-resilience.js"",
+  "network-resilience.js",
   "project-cargo-engine.js",
   "dossiers.js",
   "dossiers.css",
